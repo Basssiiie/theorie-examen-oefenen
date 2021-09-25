3284740af0c3ca782ed1a6399bc01af36bd64dae
Bas <bas-can@hotmail.com>
2021-09-26 00:17:23 +0200

    Fix multi-choice does switch between yes-no/flexible choice, + added styling

--- src/components/answers/MultipleChoice.svelte
+++ src/components/answers/MultipleChoice.svelte
@@ -5,13 +5,29 @@
 
 	export let question: ActiveQuestion;
 
-	let choices = (question.question.type === QuestionType.YesNo)
+	$: choices = (question.question.type === QuestionType.YesNo)
 		? [ $_("yes"), $_("no") ]
 		: question.localisation.choices || [];
 </script>
 
 
 {#each choices as choice, idx}
-	<input bind:group={question.answer} value={idx} id="answer{idx}" type="radio" name="answer" required>
-	<label for="answer{idx}">{choice}</label>
+	<label>
+		<input bind:group={question.answer} value={idx} id="answer{idx}" type="radio" name="answer" required>
+		{choice}
+	</label>
 {/each}
+
+
+<style>
+	label
+	{
+		display: block;
+		padding: 0.2em;
+	}
+
+	label:hover
+	{
+		background: rgba(0, 0, 0, 0.1)
+	}
+</style>
